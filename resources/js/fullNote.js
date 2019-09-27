84be8399cbba84c55e1647acec27d051c215a632
Konstantin <kulik.konstantin@gmx.de>
2019-09-27 13:02:51 +0200

    Aktualisiert

--- resources/js/fullNote.js
+++ resources/js/fullNote.js
@@ -34,44 +34,18 @@ function getInnerHTML(title, description, source, theme, color, lastUpdated, cre
   return "<div class='title-full'>" + title + `</div>
   <div class="description-full">` + description + `</div>
   <div class="source-full">
-    Quelle: <a href=".\\sources.html?source=` + encodeURIComponent(source) + "\">"
+    Quelle: <a title="Alle Zettel mit dieser Quelle anzeigen" href=".\\sources.html?source=` + encodeURIComponent(source) + "\">"
     + source + `</a>
   </div>
   <div class="theme-full">
-    Thema: <a href=".\\themes.html?theme=` + encodeURIComponent(theme) + "\">"
+    Thema: <a title="Alle Zettel mit diesem Thema anzeigen" href=".\\themes.html?theme=` + encodeURIComponent(theme) + "\">"
     + theme + `</a>
   </div>
-  <div class='color-box-full ` + color + "' title='"
-    + translateColor(lastUpdated) + `'></div>
+  <div class='color-box-full ` + color + `' title='Alle Zettel mit dieser Farbe anzeigen'></div>
   <div class='last-updated-full'>Zuletzt bearbeitet: ` + lastUpdated + `</div>
   <div class="creation-full">Erstellt: ` + creationDate + `</div>
   <div class="close-button">×</div>
   <div class="edit-button">&#9998; Zettel bearbeiten</div>`;
-}
-
-function translateColor(color){
-  switch(color){
-    case "blue":
-      return "blau";
-    case "purple":
-      return "lila";
-    case "wine":
-      return "weinrot";
-    case "red":
-      return "rot";
-    case "green":
-      return "grün";
-    case "pink":
-      return "pink";
-    case "brown":
-      return "braun";
-    case "white":
-      return "weiß";
-    case "black":
-      return "schwarz";
-    default:
-      return "";
-  }
 }
 
 export default FullNote;
